7c6ae98115ca6985248d71b173d3a3261e05f40b
Evan Cummings <evan.cummings@umontana.edu>
2014-02-07 10:12:17 -0700

    updates

--- naive_bayes/src/naive_bayes.py
+++ naive_bayes/src/naive_bayes.py
@@ -15,13 +15,21 @@ import matplotlib.gridspec as gridspec
 from time  import time
 from pylab import *
 
+mpl.rcParams['font.family']     = 'serif'
+mpl.rcParams['legend.fontsize'] = 'medium'
+
+def get_normal(x, mu, sigma):
+  return 1/(sigma * sqrt(2 * pi)) * exp(-(x - mu)**2 / (2* sigma**2))
+
 #===============================================================================
 # collect the data :
+# columns : Redness, Yellowness, Mass, Volume, Class
 
 dirc  = '../data/'
 
 # classes "lookup table" :
-classes = {1 : 'apple', 2 : 'peach', 3 : 'orange', 4 : 'lemon'}
+classes = {1 : 'apple',   2 : 'peach',      3 : 'orange', 4 : 'lemon'}
+attrib  = {0 : 'redness', 1 : 'yellowness', 2 : 'mass',   3 : 'volume'}
 
 # training set :
 train_f = open(dirc + 'fruit.csv', 'r')
@@ -35,13 +43,72 @@ test    = loadtxt(test_f,  delimiter=",", usecols=(0,1,2,3), skiprows=1)
 
 # test classes :
 test_f.seek(0)
-test_class  = loadtxt(test_f, dtype='str', delimiter=",", usecols=(4,), 
-                      skiprows=1)
+test_class  = loadtxt(test_f, delimiter=",", usecols=(4,), skiprows=1)
 
 # train classes :
 train_f.seek(0)
-train_class = loadtxt(train_f, dtype='str', delimiter=",", usecols=(4,), 
-                      skiprows=1)
+train_class = loadtxt(train_f, delimiter=",", usecols=(4,), skiprows=1)
+
+apple  = where(train_class == 1)[0]
+peach  = where(train_class == 2)[0]
+orange = where(train_class == 3)[0]
+lemon  = where(train_class == 4)[0]
+
+na = len(apple)
+np = len(peach)
+no = len(orange)
+nl = len(lemon)
+
+lens = [na, np, no, nl]
+
+def plot_dist(train, train_class, norm=True):
+  #fig  = figure(figsize=(18,12))
+  fig  = figure()
+  mus  = zeros((4,4))
+  sigs = zeros((4,4))
+  for i in range(4):
+    ax    = fig.add_subplot(220 + i+1)
+    mini  = train[:,i].min()
+    maxi  = train[:,i].max()
+    for j in range(4):
+      wj   = where(train_class == j+1)[0]
+      xj   = train[wj,i]
+      muj  = mean(xj)
+      sigj = std(xj)
+      rngi = linspace(mini, maxi, 1000)
+      lblh = classes[j+1] + ' (%i)' % (j+1)
+      lbln = r'$\mathcal{N}(\mu_%i, \sigma_%i^2)$' % (j+1, j+1)
+      ct, bins, ign = ax.hist(train[wj, i], label=lblh, alpha=0.7, normed=norm)
+      ax.plot(rngi, get_normal(rngi, muj, sigj), linewidth=2, label=lbln)
+      mus[j,i]  = muj
+      sigs[j,i] = sigj
+  
+    ax.set_title(attrib[i])
+    if i == 2:
+      leg = ax.legend()
+      leg.get_frame().set_alpha(0.5)
+    ax.grid()
+  show()
+  return mus, sigs
+
+ion()
+mus, sigs = plot_dist(train, train_class, norm=True)
+
+i = 1
+for mu, sig in zip(mus, sigs):
+  if i == 4:  nsamp = 1000
+  else:       nsamp = 10000
+  n      = len(mu)
+  b_samp = mu*ones((nsamp,n)) + sig*randn(nsamp,n)
+  if i == 1:
+    train_new       = b_samp
+    train_class_new = i*ones(nsamp)
+  else:
+    train_new = vstack((train_new, b_samp))
+    train_class_new = append(train_class_new, i*ones(nsamp))
+  i += 1
+
+mus_new, sigs_new = plot_dist(train_new, train_class_new, norm=True)
 
 
 ##===============================================================================
