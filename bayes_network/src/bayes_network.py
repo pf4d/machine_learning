ff88c449437f087a10f6ea88fe4957ded429871e
Evan Cummings <evan.cummings@umontana.edu>
2014-04-07 17:39:23 -0600

    updated bayes network

--- bayes_network/src/bayes_network.py
+++ bayes_network/src/bayes_network.py
@@ -24,7 +24,7 @@ mpl.rcParams['legend.fontsize'] = 'medium'
 dirc  = '../data/'       # directory containing data
 
 # classes "lookup table" :
-classes = {0 : 0, 1 : 1}
+classes = {0 : '0', 1 : '1'}
 attrib  = {0 : 'Storms', 
            1 : 'BusTourGroup',
            2 : 'Lightning', 
@@ -33,7 +33,7 @@ attrib  = {0 : 'Storms',
 
 # dataset :
 f    = open(dirc + 'forestFireData.csv', 'r')
-data = loadtxt(f, dtype='str', delimiter=",", skiprows=1)
+data = loadtxt(f, dtype='int', delimiter=",", skiprows=1)
 
 #===============================================================================
 # functions used to solve and plot results :
@@ -44,6 +44,60 @@ def get_normal(x, mu, sigma):
   standard deviation <sigma>.
   """
   return 1/(sigma * sqrt(2 * pi)) * exp(-(x - mu)**2 / (2* sigma**2))
+
+def plot_dist(train, nbins, name, rows, cols, plot_normal=False, 
+              log=False, norm=True):
+  """
+  Function which plots a histogram of data <train> with associated training 
+  class <train_class> with number of bins <nbins>, title <name>.  If <norm> 
+  is True, the bins are normalized.
+
+  Returns the means and standard deviations as a tuple.
+  
+  Image is saved in directory ../doc/images/.
+  """
+  train_class = train[:,-1]
+  train       = train[:,:-1]
+  vs    = unique(train_class)              # different values
+  m     = len(vs)                          # number of distinct values
+  n     = shape(train)[1]                  # number of different attributes
+  fig   = figure(figsize=(10,8))           # figure instance
+  mus   = zeros((m,n))                     # means matrix
+  sigs  = zeros((m,n))                     # standard deviations matrix
+  axi   = 100*rows + 10*cols               # format for number of plots
+  # iterate over each attribute i :
+  for i in range(n):
+    ax    = fig.add_subplot(axi + i+1)     # create a subplot
+    mini  = train[:,i].min()               # min of attribute i
+    maxi  = train[:,i].max()               # max of attribute i
+    ax.set_xlim([mini, maxi])
+    # iterate over each class j :
+    for j in range(m):
+      wj   = where(train_class == j)[0]  # indicies for class j
+      xj   = train[wj,i]                   # array of training values j
+      muj  = mean(xj)                      # mean of class j
+      sigj = std(xj)                       # standard dev of class j
+      rngi = linspace(mini, maxi, 1000)    # range of plotting normal curve
+      lblh = classes[j] + ' (%i)' % (j)
+      lbln = r'$\mathcal{N}(\mu_%i, \sigma_%i)$' % (j, j)
+      # function returns the bin counts and bins
+      ct, bins, ign = ax.hist(train[wj, i], nbins, label=lblh, alpha=0.7, 
+                              log=log, normed=norm)
+      # plot the results :
+      if plot_normal:
+        ax.plot(rngi, get_normal(rngi, muj, sigj), linewidth=2, label=lbln)
+      mus[j,i]  = muj                      # set the value of the mean
+      sigs[j,i] = sigj                     # set the value of the std. dev. 
+  
+    ax.set_title(attrib[i])                # set the title
+    if i == 2:
+      leg = ax.legend()                    # add the legend
+      leg.get_frame().set_alpha(0.5)       # transparent legend
+    ax.grid()                              # gridlines
+  tight_layout() 
+  savefig('../doc/images/' + name, dpi=300)
+  show()
+  return mus, sigs
 
 def plot_results(test_class, v, name, label=None):
   """
@@ -106,9 +160,9 @@ def classify_bayes_network(test, train, classes):
 # perform classification with k-fold cross-validation :
 
 k      = 10
-result = k_cross_validate(k, data, classify_cand_elim, classes) 
+#result = k_cross_validate(k, data, classify_cand_elim, classes) 
 
-print "percent correct: %.1f%%" % (100*average(result))
+#print "percent correct: %.1f%%" % (100*average(result))
 
 
 
