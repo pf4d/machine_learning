d1a6c23f7c032fc87da88ed112ef5cf71ec2da99
Evan Cummings <evan.cummings@umontana.edu>
2014-04-10 09:14:27 -0600

    added comments

--- bayes_network/src/bayes_network.py
+++ bayes_network/src/bayes_network.py
@@ -179,35 +179,44 @@ def cartesian(arrays, out=None):
 
 def K2(nodes, attrib, max_parents, data, g_ftn):
   """
+  K2 algorithm acts on a set of nodes <nodes> with corresponding real value 
+  dictionary <attrib>.  <max_parents> sets the max number of parents for each 
+  node, <data> is the data to operate with, and <g_ftn> either function g or
+  log_g.
   """
-  n     = len(nodes)
-  par_a = []
+  n     = len(nodes)                                 # number of parameters
+  par_a = []                                         # main parent array
 
+  # iterate through each node (attribute) :
   for i,a in zip(nodes, range(1,n+1)):
-    par_i = array([], 'int')
-    Pold  = g_ftn(i, par_i, data)
-    proc  = True
-    predi = nodes[:i]
+    par_i = array([], 'int')                         # parents for node i
+    Pold  = g_ftn(i, par_i, data)                    # rate the node
+    proc  = True                                     # ok to proceed?
+    predi = nodes[:i]                                # predicessors of node i
     
+    # while ok to proceed and parents are less than the max :
     while proc and len(par_i) < max_parents:
-      seti = setdiff1d(predi, par_i)
-      f    = []
+      seti = setdiff1d(predi, par_i)                 # predi - par_i
+      f    = []                                      # array of parental prob.
+      # iterate through the set predi - par_i
       for s in seti:
-        f.append(g_ftn(i, append(par_i, s), data))
+        f.append(g_ftn(i, append(par_i, s), data))   # append prob. to f
+      # if predi == par_i :
       if len(seti) == 0:
-        z  = array([], 'int')
+        z  = array([], 'int')                        # parents are none 
       else:
-        z  = seti[argmax(f)]
-      Pnew = g_ftn(i, append(par_i, z), data)
+        z  = seti[argmax(f)]                         # parents are max f
+      Pnew = g_ftn(i, append(par_i, z), data)        # new probability
+      # if things get better :
       if Pnew > Pold:
-        Pold  = Pnew
-        par_i = append(par_i, z)
+        Pold  = Pnew                                 # replace probabiltity
+        par_i = append(par_i, z)                     # add new parent to par_i
       else:
-        proc = False
-      parents = []
+        proc = False                                 # no longer proceed
+      parents = []                                   # form parents array 
       for p in par_i:
         parents.append(attrib[p])
-    par_a.append(par_i)
+    par_a.append(par_i)                              # add par_i to par_a
     print 'Node :', attrib[i], '\tParent of', attrib[i], ':', parents
 
   return array(par_a)
@@ -215,14 +224,17 @@ def K2(nodes, attrib, max_parents, data, g_ftn):
 
 def g(i, par_i, data):
   """
+  returns the probability that node (attribute) <i> of data matrix <data> has
+  the parents array <par_i>.
   """
-  xi    = data[:,i]
-  Vi    = unique(xi)
-  ri    = len(Vi)
-  d     = data[:,par_i]
-  m,n   = shape(d)
-  z     = len(par_i)
-
+  xi    = data[:,i]          # data column i
+  Vi    = unique(xi)         # unique values in data column i
+  ri    = len(Vi)            # number of unique values in data column i
+  d     = data[:,par_i]      # data truncated to only the parent nodes
+  m,n   = shape(d)           # size of truncated data
+  z     = len(par_i)         # number of parent nodes in par_i
+  
+  # if no parents, return the naive bayes probability :
   if z == 0:
     N_ij     = len(data)
     alpha_i_ = array([])
@@ -231,12 +243,14 @@ def g(i, par_i, data):
       alpha_i_  = append(alpha_i_, alpha_i_k)
     res = fact(ri - 1) / fact(N_ij + ri - 1) * prod(fact(alpha_i_))
   
+  # otherwise include conditional probability terms :
   else:
-    unq   = []
+    unq   = []                      # unique parent values list
+    # iterate through each dimenson :
     for j in range(n):
-      unq.append(unique(d[:,j]))
-    phi_i = cartesian(unq)
-    qi    = len(phi_i)
+      unq.append(unique(d[:,j]))    # append the unique values of parent j
+    phi_i = cartesian(unq)          # form the cross product of parental values
+    qi    = len(phi_i)              # number of possible parental combinations
     
     res   = 1
     for j in range(qi):
@@ -272,14 +286,18 @@ def logFact(n):
 
 def log_g(i, par_i, data):
   """
+  returns the probability that node (attribute) <i> of data matrix <data> has
+  the parents array <par_i> using log factorial to get around the factorial 
+  barrier.
   """
-  xi    = data[:,i]
-  Vi    = unique(xi)
-  ri    = len(Vi)
-  d     = data[:,par_i]
-  m,n   = shape(d)
-  z     = len(par_i)
+  xi    = data[:,i]          # data column i
+  Vi    = unique(xi)         # unique values in data column i
+  ri    = len(Vi)            # number of unique values in data column i
+  d     = data[:,par_i]      # data truncated to only the parent nodes
+  m,n   = shape(d)           # size of truncated data
+  z     = len(par_i)         # number of parent nodes in par_i
 
+  # if no parents, return the naive bayes probability :
   if z == 0:
     N_ij     = len(data)
     alpha_i_ = array([])
@@ -288,12 +306,14 @@ def log_g(i, par_i, data):
       alpha_i_  = append(alpha_i_, alpha_i_k)
     res = logFact(ri - 1) - logFact(N_ij + ri - 1) + sum(logFact(alpha_i_))
   
+  # otherwise include conditional probability terms :
   else:
-    unq   = []
+    unq   = []                      # unique parent values list
+    # iterate through each dimenson :
     for j in range(n):
-      unq.append(unique(d[:,j]))
-    phi_i = cartesian(unq)
-    qi    = len(phi_i)
+      unq.append(unique(d[:,j]))    # append the unique values of parent j
+    phi_i = cartesian(unq)          # form the cross product of parental values
+    qi    = len(phi_i)              # number of possible parental combinations
     
     res   = 1
     for j in range(qi):
@@ -315,35 +335,40 @@ def classify_bayes_network(test, train, classes, params):
   and dictionary of two possible classes <classes>.  The positive index of 
   <classes> is 1, while the negative index is 0.
   """
-  attrib  = params[0]
-  max_par = params[1]
-  g_ftn   = params[2]
-
-  nodes   = sort(attrib.keys())
-  network = K2(nodes, attrib, max_par, train, g_ftn)
-  n       = len(train) + 1
+  attrib  = params[0]                                  # attribute dictionary
+  max_par = params[1]                                  # max number of parents
+  g_ftn   = params[2]                                  # g function to use
+                                                       
+  nodes   = sort(attrib.keys())                        # nodes 
+  network = K2(nodes, attrib, max_par, train, g_ftn)   # B.B.S.
   
-  V_bn = []                                       # classified classes
+  V_bn = []                             # classified classes
   # iterate through each test instance and classify :
   for t in test:
+    # for each dimension in t
     for i,xi in enumerate(t):
-      pars = network[i]
+      pars = network[i]                       # form the DAG
+      # if no parents :
       if len(pars) == 0:
-        votes = []
+        votes = []                            # array of votes
+        # for each possible class :
         for c in classes.keys():
-          cnt = sum(train[:,-1] == c)
-          votes.append(cnt)
-        vi = argmax(votes)
+          cnt = sum(train[:,-1] == c)         # count up the number
+          votes.append(cnt)                   # add to tally
+        vi = argmax(votes)                    # find the max
+      # otherwise, include conditional probability terms :
       else:
-        votes = []
+        votes = []                            # array of votes
+        # for each possible class :
         for c in classes.keys():
-          d = train.copy()
+          d = train.copy()                    # copy the training data
+          # for each parent :
           for p in pars:
-            idx = where(d[:,p] == c)
-            d   = d[idx]
-          votes.append(len(d))
-        vi = argmax(votes)
-    V_bn.append(vi)
+            idx = where(d[:,p] == c)          # index where class matches
+            d   = d[idx]                      # truncate the data
+          votes.append(len(d))                # vote is the number left
+        vi = argmax(votes)                    # find the max
+    V_bn.append(vi)                           # append result
   return array(V_bn)
 
 
